c9f24876a8bc6e5531bf25b7718b60163ba63acb
AlCodersAtal <alcodersatl@gmail.com>
2024-10-28 21:31:46 +0530

    Update loginpage.php

--- docs/loginpage.php
+++ docs/loginpage.php
@@ -4,10 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Register & Login</title>
-    <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-    />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
     <link rel="stylesheet" href="css/style.css" />
     <link rel="stylesheet" href="css/loginpage.css" />
 </head>
@@ -15,17 +12,11 @@
     <nav>
         <ul class="sidebar">
             <li onclick="hideSidebar()">
-                <a href="#"
-                    ><svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="26"
-                        viewBox="0 96 960 960"
-                        width="26"
-                    >
-                        <path
-                            d="m249 849-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"
-                        /></svg
-                ></a>
+                <a href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 96 960 960" width="26">
+                        <path d="m249 849-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" />
+                    </svg>
+                </a>
             </li>
             <li><a href="index.html">Home</a></li>
             <li><a href="More info.html">More Info</a></li>
@@ -41,65 +32,36 @@
             <li class="hideOnMobile"><a href="Courses.html">Courses</a></li>
             <li class="hideOnMobile"><a href="loginpage.html">Login</a></li>
             <li class="menu-button" onclick="showSidebar()">
-                <a href="#"
-                    ><svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="26"
-                        viewBox="0 96 960 960"
-                        width="26"
-                    >
-                        <path
-                            d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"
-                        /></svg
-                ></a>
+                <a href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 96 960 960" width="26">
+                        <path d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z" />
+                    </svg>
+                </a>
             </li>
         </ul>
     </nav>
+
     <div class="container" id="signup" style="display: none">
         <h1 class="form-title">Register</h1>
         <form method="post" action="register.php">
             <div class="input-group">
                 <i class="fas fa-user"></i>
-                <input
-                    type="text"
-                    name="fName"
-                    id="fName"
-                    placeholder="First Name"
-                    required
-                />
+                <input type="text" name="fName" id="fName" placeholder="First Name" required />
                 <label for="fName">First Name</label>
             </div>
             <div class="input-group">
                 <i class="fas fa-user"></i>
-                <input
-                    type="text"
-                    name="lName"
-                    id="lName"
-                    placeholder="Last Name"
-                    required
-                />
+                <input type="text" name="lName" id="lName" placeholder="Last Name" required />
                 <label for="lName">Last Name</label>
             </div>
             <div class="input-group">
                 <i class="fas fa-envelope"></i>
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="Email"
-                    required
-                />
+                <input type="email" name="email" id="email" placeholder="Email" required />
                 <label for="email">Email</label>
             </div>
             <div class="input-group">
                 <i class="fas fa-lock"></i>
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="Password"
-                    required
-                />
+                <input type="password" name="password" id="password" placeholder="Password" required />
                 <label for="password">Password</label>
             </div>
             <input type="submit" class="btn" value="Sign Up" name="signUp" />
@@ -117,27 +79,15 @@
 
     <div class="container" id="signIn">
         <h1 class="form-title">Sign In</h1>
-        <form method="post" action="login.php"> <!-- Changed action to login.php -->
+        <form method="post" action="login.php">
             <div class="input-group">
                 <i class="fas fa-envelope"></i>
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="Email"
-                    required
-                />
+                <input type="email" name="email" id="email" placeholder="Email" required />
                 <label for="email">Email</label>
             </div>
             <div class="input-group">
                 <i class="fas fa-lock"></i>
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="Password"
-                    required
-                />
+                <input type="password" name="password" id="password" placeholder="Password" required />
                 <label for="password">Password</label>
             </div>
             <p class="recover">
@@ -155,6 +105,7 @@
             <button id="signUpButton">Sign Up</button>
         </div>
     </div>
+
     <script src="login.js"></script>
     <script src="responsive.js"></script>
 </body>
